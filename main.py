3ad53fb203122f3f1a907f88358dbf35e9a4a7ab
Antibote <yaselivoxin@gmail.com>
2025-09-26 18:17:26 +0300

    Добавлена обработка ошибок

--- main.py
+++ main.py
@@ -9,12 +9,24 @@ from auth import router as auth
 from users import router as users
 from home import router as home
 from auth import verify_auth
-
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND, HTTP_403_FORBIDDEN
 
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+
+
+@app.exception_handler(StarletteHTTPException)
+async def custom_http_exception_handler(request: Request, exc: StarletteHTTPException):
+    if exc.status_code == HTTP_401_UNAUTHORIZED:
+        return templates.TemplateResponse("errors/401.html", {"request": request}, status_code=401)
+    if exc.status_code == HTTP_403_FORBIDDEN:
+        return templates.TemplateResponse("errors/403.html", {"request": request}, status_code=403)
+    if exc.status_code == HTTP_404_NOT_FOUND:
+        return templates.TemplateResponse("errors/404.html", {"request": request}, status_code=404)
+    return templates.TemplateResponse("errors/500.html", {"request": request}, status_code=exc.status_code)
 
 @app.get("/", tags=["Главное Меню"])
 async def welcome(request: Request, error: str = None):
